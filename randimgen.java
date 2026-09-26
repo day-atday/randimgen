2ad958b88c5efd1f6b168b8a589fcc96f3aeb390
KaenS1 <adayestevezdiaz@outlook.es>
2026-09-26 13:48:03 +0100

    Delete randimgen.java

--- randimgen.java
+++
@@ -1,9 +0,0 @@
-package randimgen;
-
-public class randimgen {
-    public static void main(String[] args){
-        make_Window window = new make_Window();
-        window.MainWindow();
-        
-    }
-}
