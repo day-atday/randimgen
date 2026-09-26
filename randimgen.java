bace29c50c9bad21b69993b6e6cf0c6b60ac68a6
KaenS1 <adayestevezdiaz@outlook.es>
2026-09-26 13:47:35 +0100

    Refactored String to picture, added read Picture

---
+++ randimgen.java
@@ -0,0 +1,9 @@
+package randimgen;
+
+public class randimgen {
+    public static void main(String[] args){
+        make_Window window = new make_Window();
+        window.MainWindow();
+        
+    }
+}
